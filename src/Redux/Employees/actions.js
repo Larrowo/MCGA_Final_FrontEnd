6ebedd294b182bbea1b03daa5c14649887565682
Julián Astudillo <julian12astudillo@gmail.com>
2023-08-02 19:02:17 -0300

    Feat: redux store created
    Actions and reducer were also added

---
+++ src/Redux/Employees/actions.js
@@ -0,0 +1,89 @@
+import {
+  ADD_EMPLOYEE_SUCCESS,
+  ADD_EMPLOYEE_LOADING,
+  ADD_EMPLOYEE_ERROR,
+  GET_EMPLOYEES_SUCCESS,
+  GET_EMPLOYEES_LOADING,
+  GET_EMPLOYEES_ERROR,
+  EDIT_EMPLOYEE_SUCCESS,
+  EDIT_EMPLOYEE_LOADING,
+  EDIT_EMPLOYEE_ERROR,
+  DELETE_EMPLOYEE_SUCCESS,
+  DELETE_EMPLOYEE_LOADING,
+  DELETE_EMPLOYEE_ERROR
+} from './types'
+
+export const addEmployeeSuccess = (data) => {
+  return {
+    type: ADD_EMPLOYEE_SUCCESS,
+    payload: data
+  }
+}
+
+export const addEmployeeLoading = () => {
+  return {
+    type: ADD_EMPLOYEE_LOADING
+  }
+}
+
+export const addEmployeeError = (error) => {
+  return {
+    type: ADD_EMPLOYEE_ERROR,
+    payload: error
+  }
+}
+
+export const editEmployeeSuccess = (data) => {
+  return {
+    type: EDIT_EMPLOYEE_SUCCESS,
+    payload: data
+  }
+}
+export const editEmployeeLoading = () => {
+  return {
+    type: EDIT_EMPLOYEE_LOADING
+  }
+}
+export const editEmployeeError = (error) => {
+  return {
+    type: EDIT_EMPLOYEE_ERROR,
+    payload: error
+  }
+}
+export const deleteEmployeeSuccess = (data) => {
+  return {
+    type: DELETE_EMPLOYEE_SUCCESS,
+    payload: data
+  }
+}
+export const deleteEmployeeLoading = () => {
+  return {
+    type: DELETE_EMPLOYEE_LOADING
+  }
+}
+export const deleteEmployeeError = (error) => {
+  return {
+    type: DELETE_EMPLOYEE_ERROR,
+    payload: error
+  }
+}
+
+export const getEmployeeSuccess = (data) => {
+  return {
+    type: GET_EMPLOYEES_SUCCESS,
+    payload: data
+  }
+}
+
+export const getEmployeeLoading = (data) => {
+  return {
+    type: GET_EMPLOYEES_LOADING
+  }
+}
+
+export const getEmployeeError = (error) => {
+  return {
+    type: GET_EMPLOYEES_ERROR,
+    payload: error
+  }
+}
---
+++ src/Redux/Employees/reducer.js
@@ -0,0 +1,109 @@
+import {
+  ADD_EMPLOYEE_SUCCESS,
+  ADD_EMPLOYEE_LOADING,
+  ADD_EMPLOYEE_ERROR,
+  GET_EMPLOYEES_SUCCESS,
+  GET_EMPLOYEES_LOADING,
+  GET_EMPLOYEES_ERROR,
+  EDIT_EMPLOYEE_SUCCESS,
+  EDIT_EMPLOYEE_LOADING,
+  EDIT_EMPLOYEE_ERROR,
+  DELETE_EMPLOYEE_SUCCESS,
+  DELETE_EMPLOYEE_LOADING,
+  DELETE_EMPLOYEE_ERROR
+} from './types'
+
+const INITIAL_STATE_VALUE = {
+  users: [],
+  isLoading: false,
+  error: false,
+  message: ''
+}
+
+const employeesReducer = (state = INITIAL_STATE_VALUE, action) => {
+  switch (action.type) {
+    case ADD_EMPLOYEE_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        message: 'Loading...'
+      }
+    case ADD_EMPLOYEE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: false,
+        users: [...state.list, action.payload]
+      }
+    case ADD_EMPLOYEE_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: true,
+        message: action.payload
+      }
+    case GET_EMPLOYEES_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        message: 'LOADING EMPLOYEES'
+      }
+    case GET_EMPLOYEES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        users: action.payload
+      }
+    case GET_EMPLOYEES_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+    case EDIT_EMPLOYEE_LOADING:
+      return {
+        ...state,
+        isLoading: false,
+        message: 'Loading...'
+      }
+    case EDIT_EMPLOYEE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: false,
+        users: action.payload
+      }
+    case EDIT_EMPLOYEE_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: true,
+        message: action.payload
+      }
+    case DELETE_EMPLOYEE_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case DELETE_EMPLOYEE_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: false,
+        users: [...state.list.filter((EMPLOYEE) => EMPLOYEE._id !== action.payload)],
+        message: 'User deleted Successfully'
+      }
+    case DELETE_EMPLOYEE_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: true,
+        message: action.payload
+      }
+
+    default:
+      return state
+  }
+}
+
+export default employeesReducer
